dc5b9f2b25a0fe9fc90a0be6d5aa68e3f85ebf5a
HamzaIjaz1 <iamhamzza@gmail.com>
2018-12-09 12:18:25 +0500

    s

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Reviews from "./Components/Reviews";
 import ViewProduct from "./Components/ViewProducts";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Pane from "./Components/pane";
 import "./Components/MessagesStyle.css";
 import "./Components/Posts.css";
@@ -16,21 +16,25 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
+      
       <BrowserRouter>
-        <div>
-          <Pane />
-          <Route exact path="/" component={Shop} />
-          <Route exact path="/Shop" component={Shop} />
-          <Route path="/Messages" component={sMessages} />
-          <Route path="/Posts" component={Posts} />
-          <Route path="/Product" component={ViewProduct} />
-          <Route path="/faqs" component={Posts} />
-          <Route path="/Reviews" component={Reviews} />
-          <Route path="/Promotions" component={Posts} />
-          <Route path="/register" component={Register}/>
-          <Route path="/login" component={Login}/>
+        <div>                   
+          <Route  exact path="/" component={Login}/>
+          <Route exact path="/pane" component={Pane} ></Route>
+          <Route  path="/Shop" component={Shop} />
+          <Route  path="/Messages" component={sMessages} />
+          <Route  path="/Posts" component={Posts} />
+          <Route  path="/Product" component={ViewProduct} />
+          <Route  path="/faqs" component={Posts} />
+          <Route  path="/Reviews" component={Reviews} />
+          <Route  path="/Promotions" component={Posts} />
+          <Route  path="/register" component={Register}/>
+          <Route  path="/login" component={Login}/>
         </div>
       </BrowserRouter>
+      </React.Fragment>
+      
     );
   }
 }
